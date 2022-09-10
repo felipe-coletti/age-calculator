a42f80032c9d62ab98476ad73e2aec78d9c7a6a9
felipe-coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-09-10 10:51:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from datetime import *
 
 print("Data de nascimento")
 
-day = int(input("Dia:"))
-month = int(input("Mês:"))
-year = int(input("Ano:"))
+birthday = int(input("Dia:"))
+birthmonth = int(input("Mês:"))
+birthyear = int(input("Ano:"))
 
 birthday = [day, month, year]
+day date.today()[0]
 
 def calculate {
   
