d9018481c1a91571991081fe032f3f109b8d0525
felipe-coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-09-10 10:48:47 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from datetime import *
+
+print("Data de nascimento")
+
+day = int(input("Dia:"))
+month = int(input("Mês:"))
+year = int(input("Ano:"))
+
+birthday = [day, month, year]
+
+def calculate {
+  
+}
