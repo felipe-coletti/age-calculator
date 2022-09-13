84a73b32249d9d74ae7e644e26b501b5d25d0112
felipe-coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-09-13 11:09:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ birthmonth = int(input("Mês:"))
 birthyear = int(input("Ano:"))
 
 birthday = [day, month, year]
-day date.today()[0]
+day date.today()
 
 def calculate {
-  
+  for i in range(2, 0) {
+    age[i] = date[i] - 1 - birthday[i]
+  }
+  return age
 }
