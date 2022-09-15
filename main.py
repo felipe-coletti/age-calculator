40f1c2218b0ece88740fc1df091cfdb96472b562
felipe-coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-09-15 11:03:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ print("{} {}, {} {} e {} {}".format(
 if age[2] >= 18:
   print("Ok, divirta-se.")
 else:
-  print("Você não tem idade suficiente para acessar esta página..")
+  print("Você não tem idade suficiente para acessar esta página.")
