4a57280220dc88f38d616a1bfd7616a4fb2bdd49
felipe-coletti <83978518+felipe-coletti@users.noreply.github.com>
2022-10-06 11:21:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-# importe uma biblioteca que forneça data e hora
+# importa uma biblioteca que forneça data e hora
 from datetime import *
 
-# solicite a data de nascimento do usuário
+# solicita a data de nascimento do usuário
 print("Data de nascimento")
 
 day = int(input("Dia: "))
 month = int(input("Mês: "))
 year = int(input("Ano: "))
 
-# crie uma máscara para os dados de entrada
+# cria a máscara para a data de nascimento
 birthday = [day, month, year]
 
-# armazene a data atual do dispositivo
+# armazena a data atual do dispositivo
 date = date.today()
 
-# trate os dados
+# calcula a idade do usuário
 def calculate {
-  # calcule do maior para o menor e subtraía 1 da data atual
   years = date[2] - 1 - birthday[2]
   months = date[1] - 1 + 12 - birthday[1]
   days = date[0] - 1 + 31 - birthday[0]
   
-  # corrija os valores em excesso
+  # corrije os valores em excesso
   while days > 31 {
     months += 1
     days -= 31
@@ -31,16 +30,17 @@ def calculate {
     months -= 12
   }
   
-  # crie uma máscara para os dados tratados
+  # crie a máscara para o resultado
   return [days, months, years]
 }
 
-# armazene os dados tratados
+# armazena o resultado
 age = calculate()
 
-# trate os dados
+# palavras que podem ser usadas para exibir os dados
 words = ["dia", "mês", "ano", "dias", "meses", "anos"]
 
+# calcula o indice
 def createIndex {
   for i in range(0, 2){
     if age[i] != 1:
@@ -51,17 +51,17 @@ def createIndex {
   return numbers
 }
 
-# armazene os dados tratados
+# armazena o indice
 index = createIndex()
 
-# exiba o resultado
+# exibe a idade do usuário
 print("{} {}, {} {} e {} {}".format(
   age[2], words[index[2]],
   age[1], words[index[1]],
   age[0], words[index[0]],
 ))
 
-# você pode criar saídas diferentes para resultados diferentes
+# exibe o resultado
 if age[2] >= 18:
   print("Ok, divirta-se.")
 else:
